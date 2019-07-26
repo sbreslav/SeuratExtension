51ae6529eba2821b5b9220ca1adc0da74dad07f0
Simon Breslav <simon.breslav@autodesk.com>
2019-07-26 10:39:23 -0400

    adding clicking for 2d Graph

--- examples/02-3BoxVolumeSurfaceArea_edited.RefineryResults/72849916-9d35-4078-ba80-78de2c5693a4/analysis/playground.js
+++ examples/02-3BoxVolumeSurfaceArea_edited.RefineryResults/72849916-9d35-4078-ba80-78de2c5693a4/analysis/playground.js
@@ -6,8 +6,25 @@ function load() {
   
   var data2D = getDataMetrics2D();
   Graph2D = document.getElementById('2dGraph');
-	Plotly.plot( Graph2D, [data2D], {
-	margin: { t: 0 } } );
+	Plotly.plot( Graph2D, data2D, {
+  margin: { t: 0 } } );
+  
+  Graph2D.on('plotly_click', function(data){
+      var pts = '';
+      console.log(data);
+      var value = "";
+      for(var i=0; i < data.points.length; i++){
+              value += "id:" + data.points[i].pointIndex + "\n";
+              value += "X:" + data.points[i].x + "\n";
+              value += "Y:" + data.points[i].y + "\n";
+              for(var j=0; j < allLabels.length; j++){
+                value += allLabels[j] + ": " + allValues[data.points[i].pointIndex][j] + "\n";
+              }
+      }
+      alert('Closest point clicked:\n\n'+pts);
+      
+      document.getElementById("detailArea").value = value;
+  });
 }
 
 function round(value, decimals) {
@@ -60,31 +77,43 @@ function getDataMetrics2D() {
   // parse the csv content
   var data3d = metricsValues2D; //csv2array(csv);
 
-  var x = [];
-  var y = []; 
-  var color = [];
+  //var x = [];
+  //var y = []; 
+  var dataStream = [];
 
   // read all data
   for (var row = 0; row < data3d.length; row++) {
-    x.push(data3d[row][0]);
-    y.push(data3d[row][1]);
-    color.push(data3d[row][2]);
+    var cat = data3d[row][2];
+    var len = dataStream.length;
+    while ((len-1) < cat){
+      dataStream.push({x:[], y:[], z:[], mode: 'markers',
+      type: 'scatter',
+      name: 'Cluster '+ len,
+      marker: {
+        size: 3,
+      }});
+      len++;
+    }
+    dataStream[cat].x.push(data3d[row][0]);
+    dataStream[cat].y.push(data3d[row][1]);
+    dataStream[cat].z.push(row);
+    //color.push(data3d[row][2]);
     
 
   }
-  var maxGroup = Math.max(color);
-  var c = color.map((val) => { return (val * 255) / maxGroup; })
-  var data = {
-    mode: 'markers',
-    type: 'scatter',
-    marker: {
-      size: 3,
-      color: c//color: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
-    },
-    x: x,
-    y: y };
+  //var maxGroup = Math.max(color);
+  //var c = color; //.map((val) => { return (val * 255) / maxGroup; })
+  //var data = {
+  //  mode: 'markers',
+  //  type: 'scatter',
+  //  marker: {
+  //    size: 3,
+      //color: c//color: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
+  //  },
+  //  x: x,
+  //  y: y };
 
-  return data;
+  return dataStream;
 }
 
 
