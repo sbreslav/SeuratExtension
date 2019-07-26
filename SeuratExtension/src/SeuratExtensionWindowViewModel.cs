01e112d35f8d3145e38d18b8b8f0a00a4469ff1f
Justyna Szychowska <ucqbjsz@ucl.ac.uk>
2019-07-26 10:38:47 -0400

    perplexity added as a parameter to clustering constructor

--- SeuratExtension/src/SeuratExtensionWindowViewModel.cs
+++ SeuratExtension/src/SeuratExtensionWindowViewModel.cs
@@ -401,7 +401,7 @@ namespace SeuratExtension
             }
 
             // Run tSNE and k-Means
-            Clustering clusteredData = new Clustering(metrics, true, true, _clusterCount);
+            Clustering clusteredData = new Clustering(metrics, true, true, _clusterCount, 1.5);
 
             var resultTSNE2D = clusteredData.tSNE2D;
             var resultTSNE3D = clusteredData.tSNE3D;
